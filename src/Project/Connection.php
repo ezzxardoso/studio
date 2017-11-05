548a620353aa8fab6bcca984735e9a9d0941b9f6
Ezequiel Cardoso <ezzcardoso@icloud.com>
2017-11-05 01:08:32 -0200

    Add files via upload

---
+++ src/Project/Connection.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ */
+
+class Connection
+{
+  public static function open(){
+    use Medoo\Medoo;
+    $database = new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'eadatabase',
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => ''
+    ]);
+  return $database;
+  }
+}
+
+
+?>
